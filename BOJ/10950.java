b56dd0d74b1243d38b99fcb2f972b268fc45a2f4
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-12-30 18:07:45 +0900

    [BOJ 10950] A+B -3

---
+++ BOJ/10950.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    int T = in.nextInt();
+    for (int i = 0; i < T; i++) {
+      int A = in.nextInt();
+      int B = in.nextInt();
+
+      out.println(A + B);
+    }
+    
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream), 32768);
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
