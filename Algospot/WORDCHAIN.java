714be87bf39eb9ed516f11db2bf6272ed01fa392
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-01-30 21:45:55 +0900

    [Algospot WORDCHAIN] 단어 제한 끝말잇기

--- Algospot/WORDCHAIN.java
+++ Algospot/WORDCHAIN.java
@@ -42,11 +42,8 @@ public class Main {
     for (int i = 0; i < 26; i++) {
       for (int j = 0; j < 26; j++) {
         graph[i][j] = new ArrayList<>();
+        adj[i][j] = 0;
       }
-    }
-
-    for (int[] a1 : adj) {
-      Arrays.fill(a1, 0);
     }
 
     Arrays.fill(inDegree, 0);
@@ -68,22 +65,22 @@ public class Main {
       return "IMPOSSIBLE";
     }
 
-    ArrayList<Integer> circuit = getEuler();
+    ArrayList<Integer> euler = getEuler();
 
-    if (circuit.size() != N + 1) {
+    if (euler.size() != N + 1) {
       return "IMPOSSIBLE";
     }
 
-    Collections.reverse(circuit);
+    Collections.reverse(euler);
 
     String result = "";
-    for (int i = 0; i < circuit.size() - 1; i++) {
-      if (result.length() > 0) {
+    for (int i = 0; i < euler.size() - 1; i++) {
+      if (result.length() != 0) {
         result += " ";
       }
-      
-      result += graph[circuit.get(i)][circuit.get(i + 1)].get(0);
-      graph[circuit.get(i)][circuit.get(i + 1)].remove(0);
+
+      result += graph[euler.get(i)][euler.get(i + 1)].get(0);
+      graph[euler.get(i)][euler.get(i + 1)].remove(0);
     }
 
     return result;
@@ -94,7 +91,7 @@ public class Main {
     for (int i = 0; i < 26; i++) {
       int diff = outDegree[i] - inDegree[i];
 
-      if (diff < -1 || diff > 1) {
+      if (diff > 1 || diff < -1) {
         return false;
       }
       if (diff == 1) {
@@ -104,37 +101,38 @@ public class Main {
         minus += 1;
       }
     }
-    return (plus == 1 && minus == 1) || (plus == 0 && minus == 0);
+
+    return (plus == 0 && minus == 0) || (plus == 1 && minus == 1);
   }
 
   static ArrayList<Integer> getEuler() {
-    ArrayList<Integer> circuit = new ArrayList<>();
+    ArrayList<Integer> euler = new ArrayList<>();
 
     for (int i = 0; i < 26; i++) {
-      if (outDegree[i] == inDegree[i] + 1) {
-        dfs(i, circuit);
-        return circuit;
+      if (outDegree[i] - inDegree[i] == 1) {
+        dfs(i, euler);
+        return euler;
       }
     }
 
     for (int i = 0; i < 26; i++) {
       if (outDegree[i] > 0) {
-        dfs(i, circuit);
-        return circuit;
+        dfs(i, euler);
+        return euler;
       }
     }
 
-    return circuit;
+    return euler;
   }
 
-  static void dfs(int start, ArrayList<Integer> circuit) {
+  static void dfs(int start, ArrayList<Integer> euler) {
     for (int next = 0; next < 26; next++) {
-      while (adj[start][next] > 0) {
+      if (adj[start][next] > 0) {
         adj[start][next] -= 1;
-        dfs(next, circuit);
+        dfs(next, euler);
       }
     }
-    circuit.add(start);
+    euler.add(start);
   }
 }
 
