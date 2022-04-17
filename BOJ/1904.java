4770354c91b6f83fc81ad0e7224cd237c595eded
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-17 14:07:46 +0900

    [BOJ 1904] 01타일

---
+++ BOJ/1904.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N;
+  static int[] cache = new int[1000001];
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    N = in.nextInt();
+
+    cache[1] = 1;
+    cache[2] = 2;
+    for (int i = 3; i <= N; i++) {
+      cache[i] = (cache[i - 1] + cache[i - 2]) % 15746;
+    }
+
+    out.println(cache[N]);
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
+
