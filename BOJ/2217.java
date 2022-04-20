2a642d4a36aa3ff4e05ed0244f7addd74deac1c3
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-20 10:20:38 +0900

    [BOJ 2217] 로프

---
+++ BOJ/2217.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N;
+  static int[] cnt = new int[10001];
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    N = in.nextInt();
+    for (int i = 0; i < N; i++) {
+      cnt[in.nextInt()] += 1;
+    }
+
+    int result = 0;
+    for (int i = 1; i <= 10000; i++) {
+      if (i == 0) {
+        continue;
+      }
+
+      result = Math.max(result, N * i);
+      N -= cnt[i];
+    }
+
+    out.println(result);
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
