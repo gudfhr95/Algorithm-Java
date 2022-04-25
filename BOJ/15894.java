ca0e8eaf3246123557009a59be1c81a70ccff65c
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-25 09:10:42 +0900

    [BOJ 15894] 수학은 체육과목 입니다

---
+++ BOJ/15894.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    long n = in.nextInt();
+
+    out.println(n * 4);
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
