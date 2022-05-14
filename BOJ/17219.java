cbb94bd1ea459297131fdbb0d4ed5178105c6705
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-14 13:41:47 +0900

    [BOJ 17219] 비밀번호 찾기

---
+++ BOJ/17219.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N, M;
+  static HashMap<String, String> passwordMap = new HashMap<>();
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    N = in.nextInt();
+    M = in.nextInt();
+    for (int i = 0; i < N; i++) {
+      passwordMap.put(in.next(), in.next());
+    }
+
+    for (int i = 0; i < M; i++) {
+      out.println(passwordMap.get(in.next()));
+    }
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
