c411952de0cc15a2b38aa6a9053786a43034163c
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-20 14:33:49 +0900

    [BOJ 1715] 카드 정렬하기

---
+++ BOJ/1715.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int N;
+  static PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    N = in.nextInt();
+    for (int i = 0; i < N; i++) {
+      queue.add(in.nextInt());
+    }
+
+    int result = 0;
+    while (queue.size() > 1) {
+      int first = queue.remove();
+      int second = queue.remove();
+
+      int sum = first + second;
+      queue.add(sum);
+
+      result += sum;
+    }
+
+    out.println(result);
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
