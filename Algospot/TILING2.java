d8a935db4d9ca8417b4b0dbf9f8530117b5a70fc
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-07-05 09:43:35 +0900

    [AOJ TILING2] 타일링

---
+++ Algospot/TILING2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int C, n;
+  static int[] d = new int[101];
+
+  public static void main(String[] args) {
+    InputReader in = new InputReader(System.in);
+    PrintWriter out = new PrintWriter(System.out);
+
+    C = in.nextInt();
+
+    while (C-- > 0) {
+      n = in.nextInt();
+
+      d[1] = 1;
+      d[2] = 2;
+      for (int i = 3; i <= n; i++) {
+        d[i] = d[i - 1] + d[i - 2];
+        d[i] %= 1000000007;
+      }
+
+      out.println(d[n]);
+    }
+
+    out.flush();
+  }
+}
+
+class InputReader {
+
+  BufferedReader reader;
+  StringTokenizer tokenizer;
+
+  public InputReader(InputStream stream) {
+    reader = new BufferedReader(new InputStreamReader(stream));
+    tokenizer = null;
+  }
+
+  public String next() {
+    while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+      try {
+        tokenizer = new StringTokenizer(reader.readLine());
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return tokenizer.nextToken();
+  }
+
+  public int nextInt() {
+    return Integer.parseInt(next());
+  }
+}
