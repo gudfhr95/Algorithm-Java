ba524201299c60112047d383285e666af2390ed0
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-07-14 11:18:06 +0900

    [BOJ 25083] 새싹

---
+++ BOJ/25083.java
@@ -0,0 +1,17 @@
+import java.io.PrintWriter;
+
+public class Main {
+
+  public static void main(String[] args) {
+    PrintWriter out = new PrintWriter(System.out);
+
+    out.printf("         ,r'\"7\n"
+        + "r`-_   ,'  ,/\n"
+        + " \\. \". L_r'\n"
+        + "   `~\\/\n"
+        + "      |\n"
+        + "      |");
+
+    out.flush();
+  }
+}
