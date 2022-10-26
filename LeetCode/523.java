0a682ff3e68524129f6945d56b79f80c12c859dd
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-10-26 11:03:20 +0900

    [LeetCode 523] Continuous Subarray Sum

---
+++ LeetCode/523.java
@@ -0,0 +1,21 @@
+class Solution {
+
+  public boolean checkSubarraySum(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    map.put(0, 0);
+
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum += nums[i];
+
+      if (!map.containsKey(sum % k)) {
+        map.put(sum % k, i + 1);
+      } else if (i > map.get(sum % k)) {
+        return true;
+      }
+
+    }
+
+    return false;
+  }
+}
